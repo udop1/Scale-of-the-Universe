2fe5b54fb4206133ee87657762f00841eedd3b75
udop1 <adamtaylor2001@live.co.uk>
2022-10-17 12:43:02 +0100

    Custom planet divider distance

--- planets.js
+++ planets.js
@@ -1,34 +1,39 @@
 //https://www.solarsystemscope.com/spacepedia/handbook/
 //https://www.solarsystemscope.com/textures/
-const planetsData = [
+const planetsData = [ //Name, Scale, Distance Divider, Texture, Info Texxt
     [
         "Earth",
         1,
+        10,
         "./images/2k_earth_daymap.jpg",
         "Info about the Earth"
     ],
     [
         "Moon",
         0.25,
+        1,
         "./images/2k_moon.jpg",
         "Info about the Moon"
     ],
     [
         "Mars",
         0.5,
+        5,
         "./images/2k_mars.jpg",
         "Info about Mars"
     ],
     [
         "Jupiter",
         11,
+        10,
         "./images/2k_jupiter.jpg",
         "Info about Jupiter"
     ],
     [
         "Sun",
         109,
+        10,
         "./images/2k_sun.jpg",
-        "The Sun, also referred to as \"Sol\", is the star at the center of the Solar System. Sun's mass accounts for some 99.86% of the total mass of the Solar System.\nComposition\nRoughly three-quarters of the Sun's mass is hydrogen, with the rest mostly helium. Only 1.69% of the Sun (which is still 5,628 times the mass of the Earth) is made up of heavier elements like carbon, iron, neon and oxygen.\nDistance\nThe mean distance of the Sun from the Earth is approximately 149.6 million kilometres (1 AU). On average, it takes light from the Sun about 8 minutes and 19 seconds to reach the Earth.\nGalaxy\nOnce thought to be a relatively insignificant star, the Sun is now considered to be brighter than about 85% of the stars in the Milky Way galaxy. It is located about two thirds of the way out from the centre of the Milky Way."
+        "The Sun\n\nThe Sun, also referred to as \"Sol\", is the star at the center of the Solar System. Sun\'s mass accounts for some 99.86% of the total mass of the Solar System.\n\nComposition\nRoughly three-quarters of the Sun\'s mass is hydrogen, with the rest mostly helium. Only 1.69% of the Sun (which is still 5,628 times the mass of the Earth) is made up of heavier elements like carbon, iron, neon and oxygen.\n\nDistance\nThe mean distance of the Sun from the Earth is approximately 149.6 million kilometres (1 AU). On average, it takes light from the Sun about 8 minutes and 19 seconds to reach the Earth.\n\nGalaxy\nOnce thought to be a relatively insignificant star, the Sun is now considered to be brighter than about 85% of the stars in the Milky Way galaxy. It is located about two thirds of the way out from the centre of the Milky Way."
     ]
 ];
