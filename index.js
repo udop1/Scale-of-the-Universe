054477381e7f6c74b0e132a7fa6039c273891e06
udop1 <adamtaylor2001@live.co.uk>
2022-11-08 13:50:11 +0000

    Update index.js

--- index.js
+++ index.js
@@ -287,11 +287,11 @@ var createScene = function() {
             });
         }
     });
-    
+
 
     //Audio //https://doc.babylonjs.com/features/featuresDeepDive/audio/playingSoundsMusic
-    /*var solBGM = new BABYLON.Sound("solSystemBGM", "./sound/Dreamy Flashback.mp3", scene, null, {loop: true, autoplay: true, spatialSound: true, distanceModel: "linear", maxDistance: 100});
-    solBGM.setPosition(new BABYLON.Vector3(planetMeshes[0].position.x, 0, 0));*/
+    /*var solBGM = new BABYLON.Sound("solSystemBGM", "./sound/Dreamy Flashback.mp3", scene, null, {loop: true, autoplay: true, spatialSound: true, distanceModel: "linear", maxDistance: 100, panningModel: "HRTF"});
+    solBGM.attachToMesh(planetMeshes[4]);*/
 
     var audioRunning = false;
     btnAudio.onPointerClickObservable.add(function() {
